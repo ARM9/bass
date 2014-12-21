7fa1271d1904bbccb136bb9fee75305f892dd4c7
ARM9 <toast.dslua@gmail.com>
2014-12-21 12:29:46 +0100

    cx4 tbd

---
+++ .test/cx4_test/main.asm
@@ -0,0 +1,42 @@
+    arch snes.cx4
+
+//db "cx4"
+cx4_start:
+    nop
+    stop
+
+    unknown0
+
+    jmp $7f
+    jmp prg_page:70
+    jmpeq prg_page:$80
+    jmpeq $80
+    jmpge prg_page:$ff
+    jmpge $FF
+    jmpmi prg_page:$ff
+    jmpmi $7f
+
+    unknown1
+
+    finish ext_dta
+
+    unknown2
+
+    skiplt
+    skipge
+    skipne
+    skipeq
+    skippl
+    skipmi
+
+    //call
+    unknown3
+
+    ret
+
+    inc ext_ptr
+
+    unknown4
+
+    cmp a,a
+// vim:ft=bass
