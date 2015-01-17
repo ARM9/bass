2b2cdc9c917121a7bb6e1571dcfe9858e6221cfb
PeterLemon <krom@gbadev.org>
2015-01-17 05:34:36 +0000

    N64 RDP Arch Update

--- .test/rdp_test/RDPTest.asm
+++ .test/rdp_test/RDPTest.asm
@@ -5,27 +5,36 @@ endian msb
 // RDP Commands:
 No_Op
 
+Fill_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Fill_ZBuffer_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Texture_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Texture_ZBuffer_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Shade_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Shade_ZBuffer_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Shade_Texture_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+Shade_Texture_Z_Buffer_Triangle 1,2,3,4,5,6, 7,8,9,10, 11,12,13,14, 15,16,17,18 // lft,level,tile,yl,ym,yh, xl,xlf,dxldy,dxldyf, xh,xhf,dxhdy,dxhdyf, xm,xmf,dxmdy,dxmdyf
+//Shade_Coefficients
+Texture_Coefficients 1,2,3, 4,5,6, 7,8,9, 10,11,12, 13,14,15, 16,17,18, 19,20,21, 22,23,24 // s,t,w, dsdx,dtdx,dwdx, sf,tf,wf, dsdxf,dtdxf,dwdxf, dsde,dtde,dwde, dsdy,dtdy,dwdy, dsdef,dtdef,dwdef, dsdyf,dtdyf,dwdyf
 ZBuffer_Coefficients 1,2, 3,4, 5,6, 7,8 // z,zf, dzdx,dzdxf, dzde,dzdef, dzdy,dzdyf
 
 Texture_Rectangle 1,2, 3, 4,5, 6,7, 8,9 // xl,yl, tile, xh,yh, s,t, dsdx,dtdy
 Texture_Rectangle_Flip 1,2, 3, 4,5, 6,7, 8,9 // xl,yl, tile, xh,yh, s,t, dsdx,dtdy
-
 Sync_Load
 Sync_Pipe
 Sync_Tile
 Sync_Full
-
 Set_Key_GB 1,2, 3,4, 5,6 // wg,wb, cg,sg, cb,sb
 Set_Key_R 1,2,3 // wr,cr,sr
 Set_Convert 1,2,3,4,5,6 // k0,k1,k2,k3,k4,k5
 Set_Scissor 1,2, 1,1, 3,4 // xh,yh, f,o, xl,yl
 Set_Prim_Depth 1,2 // pz,pdz
-
+//Set_Other_Modes
 Load_Tlut 1,2, 3, 4,5 // sl,tl, tile, sh,th
+
 Set_Tile_Size 1,2, 3, 4,5 // sl,tl, tile, sh,th
 Load_Block 1,2, 3, 4,5 // sl,tl, tile, sh,dxt
 Load_Tile 1,2, 3, 4,5 // sl,tl, tile, sh,th
-
+Set_Tile 1,2,3, $1FF, 4,5, 1,1,15,15, 1,1,15,15 // format,size,width, tmem base, tile,palette, clamp t,mirror t,mask t,shift t, clamp s,mirror s,mask s,shift s
 Fill_Rectangle 1,2, 3,4 // xl,yl, xh,yh
 Set_Fill_Color $FFEEDDCC // packed fill color
 Set_Fog_Color $FFEEDDCC // fog color
