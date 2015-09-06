223aae92b1faf2d9986d75e39a5c6be248a26bc6
ARM9 <toast.dslua@gmail.com>
2015-09-06 16:50:34 +0200

    Better branch_test.asm

--- bass/.test/mips_test/branch_test/branch_test.asm
+++ bass/.test/mips_test/branch_test/branch_test.asm
@@ -1,4 +1,5 @@
 arch n64.cpu
+endian msb
 
 macro align(size) {
   while (pc() % {size}) {
@@ -6,9 +7,7 @@ macro align(size) {
   }
 }
 
-endian msb
-instrument "beqtest *01,*05,*18 ; %000100 >>04a >>03a >>02a >>01a ~a ~b +4>>02c"
-//instrument "j *01 ; %000010 >>27a >>26a >>25a >>24a >>23a >>22a >>21a >>20a >>19a >>18a >>17a >>16a >>15a >>14a >>13a >>12a >>11a >>10a >>09a >>08a >>07a >>06a >>05a >>04a >>03a >>02a"
+base 0xf0000000
 
 _start:
     beq 2, 2,_end
@@ -19,18 +18,24 @@ _start:
     beq 2, 2, _start
     nop
     beq 2, 2, _start
-    align(4)
 _end:
     //fill (1<<20)
     origin 0x120000
-    align(4)
 fo:
-    j _start            // works
-    nop
-    j _end+$2000000-1   // works
-    nop
-    j _end+$4000000-1   // msb should be $09 is $08
-    nop
-    j fo-1              // works
+    j fo
+    j fo-1
+    j _end+$2000000-1
+    j _end+$4000000-1
+    j $FFFFFFF          // -> 0xFFFFFFC
+    j $FFFFFFFFFFFFFF   // shouldn't overflow anymore
+    j $F
+    //jt fo-1
+    jal fo
+    jal fo-1
+    jal _end+$2000000-1
+    jal _end+$4000000-1
+    jal $FFFFFFF
+    jal $FFFFFFFFFFFFFF
+    jal $F
 
 // vim:ft=mips
